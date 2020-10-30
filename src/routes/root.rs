49bab02902515f6a4f7754e573af6f087988a759
PurePeace <940857703@qq.com>
2020-10-30 12:03:20 +0800

    add: win cross-compile to linux

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -3,6 +3,16 @@ use actix_web::{get, HttpResponse, Responder};
 /// GET "/"
 #[get("/")]
 pub async fn index() -> impl Responder {
-    let contents = "Hello root!";
+    let contents = r#"<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Hello!</title>
+      </head>
+      <body>
+        <h1>Hello!</h1>
+        <p>Hi from Rust</p>
+      </body>
+    </html>"#;
     HttpResponse::Ok().body(contents)
 }
