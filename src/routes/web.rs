16b7a9c1fbd0abce0b0c8d35ca58ea56c5d110bb
Pure-Peace <940857703@qq.com>
2020-11-04 11:59:58 +0800

    add: struct comments

--- src/routes/web.rs
+++ src/routes/web.rs
@@ -7,6 +7,19 @@ use crate::utils;
 use actix_multipart::Multipart;
 use std::borrow::BorrowMut;
 
+/// Query Data
+///
+/// GET /web/lastfm.php
+///
+/// ```
+/// Lastfm {
+///     b: String = beatmap ban,
+///     action: String,
+///     us: String = username,
+///     ha: String = password hash,
+/// }
+///
+/// ```
 #[derive(Debug, Deserialize)]
 pub struct Lastfm {
     b: String,
@@ -15,6 +28,18 @@ pub struct Lastfm {
     ha: String,
 }
 
+/// Query Data
+///
+/// GET /web/check-updates.php
+///
+/// ```
+/// CheckUpdates {
+///     action: String = [check, path, error],
+///     stream: String = [cuttingedge, stable40, beta40, stable],
+///     time: String = timeStamp,
+/// }
+///
+/// ```
 #[derive(Debug, Deserialize)]
 pub struct CheckUpdates {
     action: String,
@@ -22,6 +47,21 @@ pub struct CheckUpdates {
     time: String,
 }
 
+/// Query Data
+///
+/// GET /web/bancho_connect.php
+///
+/// ```
+/// BanchoConnect {
+///     v: String = osu version,
+///     u: String = username,
+///     h: String = password hash,
+///     fx: String = donet env,
+///     ch: String = hardware hashes,
+///     retry: i32 = retries,
+/// }
+///
+/// ```
 #[derive(Debug, Deserialize)]
 pub struct BanchoConnect {
     v: String,
@@ -32,6 +72,18 @@ pub struct BanchoConnect {
     retry: i32,
 }
 
+/// Multipart Form-data
+///
+/// POST /web/osu-session.php
+///
+/// ```
+/// OsuSession {
+///     u: String = username,
+///     h: String = password hash,
+///     action: String = [check, submit],
+/// }
+///
+/// ```
 #[derive(Debug, Deserialize)]
 pub struct OsuSession {
     u: String,
@@ -39,6 +91,34 @@ pub struct OsuSession {
     action: String,
 }
 
+/// Multipart Form-data
+///
+/// POST /web/osu-error.php
+///
+/// ```
+/// OsuError {
+///     u: String = username,
+///     p: String = password hash,
+///     i: i32,
+///     osumode: String = [Menu],
+///     gamemode: String = [Osu, Taiko, Mania, Catch],
+///     gametime: u32,
+///     audiotime: u32,
+///     culture: String = [zh-CN],
+///     beatmap_id: u32,
+///     beatmap_checksum: String,
+///     exception: String = [System.Exception],
+///     feedback: String = [update error],
+///     stacktrace: String,
+///     soft: String = [True, False],
+///     beatmap_count: u32,
+///     compatibility: u32,
+///     version: String = osu version,
+///     exehash: String,
+///     config: String = osu config(ini),
+/// }
+///
+/// ```
 #[derive(Debug, Deserialize)]
 pub struct OsuError {
     u: String,
