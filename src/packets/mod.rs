40d8289670f41cc73a55d5ef282de74682d8f348
PurePeace <940857703@qq.com>
2020-11-11 22:50:17 +0800

    refactor: improve performance, fix correct packet length

--- src/packets/mod.rs
+++ src/packets/mod.rs
@@ -2,35 +2,49 @@ use actix_web::web::Bytes;
 
 pub mod id;
 
+pub fn notification(msg: &str) -> Vec<u8> {
+    let mut packet = new(id::BANCHO_NOTIFICATION);
+    packet.extend(write_string(msg));
+    output(packet)
+}
+
+pub fn match_join_fail() -> Vec<u8> {
+    output(new(id::BANCHO_MATCH_JOIN_FAIL))
+}
+
 /// Create a empty packets
 pub fn empty() -> Vec<u8> {
-    Vec::with_capacity(7)
+    Vec::with_capacity(11)
 }
 
 /// Initial a packets by id
+///
+/// Packets posit:
+/// ```
+/// [0..=2]: packet id
+/// [3..=6]: packet length
+/// [7..=N]: data length(uleb128)  + data
+/// ```
 pub fn new(packet_id: u8) -> Vec<u8> {
-    vec![packet_id, 0, 0]
+    vec![packet_id, 0, 0, 0, 0, 0, 0]
 }
 
-pub fn output(mut ret: Vec<u8>) -> Vec<u8> {
-    ret.splice(3..3, vec![(ret.len() - 3) as u8, 0, 0, 0]);
-    ret
+/// Add packet length
+pub fn output(mut packet: Vec<u8>) -> Vec<u8> {
+    for (index, value) in ((packet.len() - 7) as i32).to_le_bytes().iter().enumerate() {
+        packet[3 + index] = *value;
+    }
+    packet
 }
 
-pub fn notification(msg: &str) -> Vec<u8> {
-    let mut ret = new(id::BANCHO_NOTIFICATION);
-    ret.extend(write_string(msg));
-    output(ret)
-}
-
+/// Write string packet
 pub fn write_string(string: &str) -> Vec<u8> {
-    let byte_data = string.as_bytes();
     let byte_length = string.len();
-    let mut data: Vec<u8> = Vec::with_capacity(byte_length + 5);
+    let mut data: Vec<u8> = Vec::with_capacity(byte_length + 3);
     if byte_length > 0 {
         data.push(11); // 0x0b, means not empty
         data.extend(uleb128(byte_length as u32));
-        data.extend(byte_data);
+        data.extend(string.as_bytes());
     } else {
         data.push(0); // 0x00, means empty
     }
@@ -39,7 +53,7 @@ pub fn write_string(string: &str) -> Vec<u8> {
 
 /// Unsigned to uleb128
 fn uleb128(mut unsigned: u32) -> Vec<u8> {
-    let mut data: Vec<u8> = Vec::new();
+    let mut data: Vec<u8> = Vec::with_capacity(2);
     while unsigned >= 0x80 {
         data.push(((unsigned & 0x7f) | 0x80) as u8);
         unsigned >>= 7;
