0150d960f79b10f64ae1553b6bd854a4d95a00ac
PurePeace <940857703@qq.com>
2020-11-21 05:10:33 +0800

    add: sql*

---
+++ sql/bancho.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : pg-localhost
+ Source Server Type    : PostgreSQL
+ Source Server Version : 110009
+ Source Host           : localhost:5432
+ Source Catalog        : peace
+ Source Schema         : bancho
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 110009
+ File Encoding         : 65001
+
+ Date: 21/11/2020 05:08:56
+*/
+
