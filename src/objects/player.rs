709f63fcbfbef8f841f5c2c81aa61533c706e546
PurePeace <940857703@qq.com>
2020-11-24 19:44:16 +0800

    add: PacketData queue

--- src/objects/player.rs
+++ src/objects/player.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
-use async_std::sync::RwLock;
+use crate::types::PacketData;
+use async_std::sync::{Mutex, RwLock};
 use chrono::prelude::{DateTime, Local};
+use queue::Queue;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize)]
@@ -11,7 +13,7 @@ pub struct PlayerBase {
     pub country: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Player {
     pub id: i32,
     pub name: String,
@@ -19,8 +21,9 @@ pub struct Player {
     pub country: String,
     pub osu_version: String,
     pub utc_offset: i32,
+    pub queue: Mutex<Queue<PacketData>>,
     pub login_time: DateTime<Local>,
-    pub last_active_time: DateTime<Local>
+    pub last_active_time: DateTime<Local>,
 }
 
 impl Player {
@@ -37,6 +40,7 @@ impl Player {
             country: base.country,
             osu_version,
             utc_offset,
+            queue: Mutex::new(Queue::new()),
             login_time: now_time,
             last_active_time: now_time,
         }
@@ -44,5 +48,32 @@ impl Player {
 
     pub fn update_active(&mut self) {
         self.last_active_time = Local::now();
+    }
+
+    /// Enqueue a packet into queue, returns the length of queue
+    async fn enqueue(&self, packet_data: PacketData) -> Result<usize, ()> {
+        self.queue.lock().await.queue(packet_data)
+    }
+
+    async fn dequeue(&self) -> Option<PacketData> {
+        self.queue.lock().await.dequeue()
+    }
+
+    /// Get the queue data as vec, readonly
+    async fn queue_data(&self) -> Vec<PacketData> {
+        self.queue.lock().await.vec().clone()
+    }
+
+    /// Get the queue size
+    async fn queue_len(&self) -> usize {
+        self.queue.lock().await.len()
+    }
+
+    async fn queue_peek(&self) -> Option<PacketData> {
+        self.queue.lock().await.peek()
+    }
+
+    async fn queue_is_empty(&self) -> bool {
+        self.queue.lock().await.is_empty()
     }
 }
