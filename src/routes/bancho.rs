ca0546e6c74bf581e67aa5ed3efc572baee9545f
PurePeace <940857703@qq.com>
2020-11-29 00:21:23 +0800

    add: limitation of failed logins (each request ip)

--- src/routes/bancho.rs
+++ src/routes/bancho.rs
@@ -6,9 +6,16 @@ use actix_web::{HttpRequest, HttpResponse, Responder};
 use async_std::sync::RwLock;
 use prometheus::IntCounterVec;
 
-use crate::objects::{Player, PlayerSessions};
 use crate::utils;
+use crate::{
+    constants::packets::LoginReply,
+    objects::{Player, PlayerSessions},
+    packets,
+};
 use crate::{database::Database, handlers::bancho};
+
+const MAX_FAILED_COUNT: i32 = 4;
+const EXPIRE_SECS: i32 = 300;
 
 pub async fn get(req: HttpRequest, body: Bytes, counter: Data<IntCounterVec>) -> impl Responder {
     counter
@@ -45,21 +52,52 @@ pub async fn post(
 
     // If not login
     if !headers.contains_key("osu-token") {
+        let failed_key = format!("{}-bancho_login_failed", &request_ip);
+        let failed_count = database.redis.get(failed_key.clone()).await.unwrap_or(0);
+        // Too many failed in 300s, refuse login
+        if failed_count > MAX_FAILED_COUNT {
+            warn!(
+                "ip: {} login refused, beacuse too many login failed_count({}) in {}s;",
+                request_ip, failed_count, EXPIRE_SECS
+            );
+            return HttpResponse::Ok()
+                .set_header("cho-token", "login_refused")
+                .set_header("cho-protocol", "19")
+                .body(
+                    packets::PacketBuilder::from(packets::login_reply(LoginReply::ServerError))
+                        .write_out(),
+                );
+        }
+
+        // Login handle
         let (resp_body, token) = bancho::login(
             req,
             &body,
-            request_ip,
+            &request_ip,
             osu_version,
             &database,
             player_sessions,
             &counter,
         )
         .await;
+
+        // Login failed handle
         if token == "login_failed" {
+            // Record login failed
             counter
                 .with_label_values(&["/bancho", "post", "login.failed"])
                 .inc();
+            // Increase failed count for this ip
+            let failed_count: i32 = database.redis.query("INCR", &[failed_key.clone()]).await;
+            let _ = database.redis.expire(failed_key, EXPIRE_SECS).await;
+            if failed_count > MAX_FAILED_COUNT {
+                warn!(
+                    "ip: {} login failed, count: {}, will temporarily restrict their login",
+                    &request_ip, failed_count
+                );
+            }
         }
+
         return HttpResponse::Ok()
             .set_header("cho-token", token)
             .set_header("cho-protocol", "19")
