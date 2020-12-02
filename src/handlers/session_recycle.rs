50ab1108e79f7b31985dfc83de0d99c11b678136
PurePeace <940857703@qq.com>
2020-12-03 03:43:28 +0800

    add: more info about recycling will be displayed

--- src/handlers/session_recycle.rs
+++ src/handlers/session_recycle.rs
@@ -10,6 +10,9 @@ pub async fn session_recycle_handler(
     player_sessions: &Data<RwLock<PlayerSessions>>,
     session_timeout: i64,
 ) {
+    let mut recycled_sessions_count = 0;
+    let session_recycle_start = std::time::Instant::now();
+
     // Get read lock
     let sessions = player_sessions.read().await;
     let session_map = sessions.map.read().await;
@@ -24,17 +27,22 @@ pub async fn session_recycle_handler(
         .iter()
         .map(|(token, player)| (token.to_string(), PlayerData::from(player)))
         .collect();
+    // Drop lock before handled
     drop(session_map);
     for (token, player) in map_data {
         if Local::now().timestamp() - player.last_active_time.timestamp() > session_timeout {
             match sessions.logout(token).await {
-                Some((_token, player)) => warn!(
-                    "deactive user {}({}) has been recycled.",
-                    player.name, player.id
-                ),
+                Some((_token, player)) => {
+                    recycled_sessions_count += 1;
+                    warn!(
+                        "deactive user {}({}) has been recycled.",
+                        player.name, player.id
+                    )
+                },
                 None => {}
             }
         }
     }
-    debug!("session recycle task complete!");
+    let session_recycle_end = session_recycle_start.elapsed();
+    debug!("session recycle task complete in {:.2?}; recycled: {} sessions.", session_recycle_end, recycled_sessions_count);
 }
