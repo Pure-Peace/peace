fc929ad346b6f6cdb2deb06c1ceb154055fcb667
PurePeace <940857703@qq.com>
2021-02-15 02:05:33 +0800

    refactor & add: logger can exclude modules now

--- src/settings/logger.rs
+++ src/settings/logger.rs
@@ -6,21 +6,19 @@ use log::LevelFilter;
 impl Logger {
     /// Init logger
     pub fn init(cfg: &Config) {
-        let level = cfg.get_str("logger.level").unwrap_or("info".to_string());
-        let env = env_logger::Env::default()
-            // Try to get LOG_FILTER from .env,
-            // If not exists, try use cfg
-            .filter_or(
-                "LOG_FILTER",
-                cfg.get_str(&format!("logger.mode.{}", &level))
-                    .unwrap_or("info".to_string()),
-            );
+        let log_level = cfg.get_str("logger.level").unwrap_or("debug".to_string());
+        let exclude_modules: Vec<String> = cfg.get("logger.exclude_modules").unwrap_or_default();
 
-        //env_logger::init_from_env(env);
-        env_logger::Builder::from_env(env)
-            // Disabled maxminddb debug log (so many wtf)
-            .filter_module("maxminddb::decoder", LevelFilter::Warn)
-            .format_timestamp_millis()
-            .init();
+        let env = env_logger::Env::default().filter_or(
+            "LOG_FILTER",
+            cfg.get_str(&format!("logger.mode.{}", &log_level))
+                .unwrap_or("info".to_string()),
+        );
+
+        let mut builder = env_logger::Builder::from_env(env);
+        for module in &exclude_modules {
+            builder.filter_module(module, LevelFilter::Warn);
+        }
+        builder.format_timestamp_millis().init();
     }
 }
