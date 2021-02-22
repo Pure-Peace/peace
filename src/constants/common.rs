90c29640e7199354fb6889ed65721bb0c824e8be
PurePeace <940857703@qq.com>
2021-02-22 22:27:39 +0800

    update: version

--- src/constants/common.rs
+++ src/constants/common.rs
@@ -1,5 +1,5 @@
-pub const DB_VERSION: &str = "0.2.0";
-pub const PEACE_VERSION: &str = "0.2.0";
+pub const DB_VERSION: &str = "0.2.1";
+pub const PEACE_VERSION: &str = "0.2.1";
 pub const PEACE_BANNER: &str = r#"
 
    .-.                            
