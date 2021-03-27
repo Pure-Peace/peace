f9c76f0a34d5dccb8213c14bb99f5fd36e256ee3
Pure-Peace <940857703@qq.com>
2021-03-28 02:34:17 +0800

    refactors: routes/debug

--- src/routes/debug.rs
+++ src/routes/debug.rs
@@ -1,14 +1,12 @@
-use crate::{
-    database::Database, objects::OsuApi, settings::bancho::BanchoConfig, types::ChannelList,
-};
-use crate::{objects::PlayerSessions, utils};
+use crate::utils;
+use crate::{database::Database, objects::Bancho};
 
 use actix_web::{
     dev::Server,
     web::{Data, Path},
 };
 use actix_web::{get, HttpResponse, Responder};
-use async_std::{channel::Sender, sync::RwLock};
+use async_std::channel::Sender;
 use maxminddb::Reader;
 use memmap::Mmap;
 
@@ -43,9 +41,9 @@ pub async fn test_redis(database: Data<Database>) -> impl Responder {
 
 /// GET "/test_async_lock"
 #[get("/test_async_lock")]
-pub async fn test_async_lock(player_sessions: Data<RwLock<PlayerSessions>>) -> impl Responder {
+pub async fn test_async_lock(bancho: Data<Bancho>) -> impl Responder {
     let start = Instant::now();
-    let player_sessions = player_sessions.read().await;
+    let player_sessions = bancho.player_sessions.read().await;
     let _map = player_sessions.token_map.read().await;
     let end = start.elapsed();
     HttpResponse::Ok()
@@ -55,12 +53,15 @@ pub async fn test_async_lock(player_sessions: Data<RwLock<PlayerSessions>>) -> i
 
 /// GET "/test_player_read"
 #[get("/test_player_read/{token}")]
-pub async fn test_player_read(
-    token: Path<String>,
-    player_sessions: Data<RwLock<PlayerSessions>>,
-) -> impl Responder {
+pub async fn test_player_read(token: Path<String>, bancho: Data<Bancho>) -> impl Responder {
     let start = Instant::now();
-    let player_info = match player_sessions.read().await.get_player_data(&token.0).await {
+    let player_info = match bancho
+        .player_sessions
+        .read()
+        .await
+        .get_player_data(&token.0)
+        .await
+    {
         Some(player_data) => format!("{:?}", player_data),
         None => "non this player".to_string(),
     };
@@ -70,12 +71,9 @@ pub async fn test_player_read(
 
 /// GET "/test_player_money_add"
 #[get("/test_player_money_add/{token}")]
-pub async fn test_player_money_add(
-    token: Path<String>,
-    player_sessions: Data<RwLock<PlayerSessions>>,
-) -> impl Responder {
+pub async fn test_player_money_add(token: Path<String>, bancho: Data<Bancho>) -> impl Responder {
     let start = Instant::now();
-    let player_sessions = player_sessions.read().await;
+    let player_sessions = bancho.player_sessions.read().await;
     let map = player_sessions.token_map.read().await;
     let player_info = match map.get(&token.0) {
         Some(player) => {
@@ -91,12 +89,9 @@ pub async fn test_player_money_add(
 
 /// GET "/test_player_money_reduce"
 #[get("/test_player_money_reduce/{token}")]
-pub async fn test_player_money_reduce(
-    token: Path<String>,
-    player_sessions: Data<RwLock<PlayerSessions>>,
-) -> impl Responder {
+pub async fn test_player_money_reduce(token: Path<String>, bancho: Data<Bancho>) -> impl Responder {
     let start = Instant::now();
-    let player_sessions = player_sessions.read().await;
+    let player_sessions = bancho.player_sessions.read().await;
     let map = player_sessions.token_map.read().await;
     let player_info = match map.get(&token.0) {
         Some(player) => {
@@ -113,10 +108,11 @@ pub async fn test_player_money_reduce(
 #[get("/test_player_money_reduce_special/{token}")]
 pub async fn test_player_money_reduce_special(
     token: Path<String>,
-    player_sessions: Data<RwLock<PlayerSessions>>,
+    bancho: Data<Bancho>,
 ) -> impl Responder {
     let start = Instant::now();
-    let player_info = player_sessions
+    let player_info = bancho
+        .player_sessions
         .read()
         .await
         .handle_player_get(&token.0, |_player| Some(()) /* (*player).money -= 1 */)
@@ -127,15 +123,15 @@ pub async fn test_player_money_reduce_special(
 
 /// GET "/player_sessions_all"
 #[get("/player_sessions_all")]
-pub async fn player_sessions_all(player_sessions: Data<RwLock<PlayerSessions>>) -> impl Responder {
-    HttpResponse::Ok().body(player_sessions.read().await.map_to_string().await)
+pub async fn player_sessions_all(bancho: Data<Bancho>) -> impl Responder {
+    HttpResponse::Ok().body(bancho.player_sessions.read().await.map_to_string().await)
 }
 
 /// GET "/player_maps_info"
 #[get("/player_maps_info")]
-pub async fn player_maps_info(player_sessions: Data<RwLock<PlayerSessions>>) -> impl Responder {
+pub async fn player_maps_info(bancho: Data<Bancho>) -> impl Responder {
     let start = Instant::now();
-    let maps = player_sessions.read().await;
+    let maps = bancho.player_sessions.read().await;
     let (token_map, id_session_map, name_session_map) = (
         maps.token_map.read().await,
         maps.id_session_map.read().await,
@@ -152,22 +148,19 @@ pub async fn player_maps_info(player_sessions: Data<RwLock<PlayerSessions>>) -> 
 
 /// GET "/player_channels_all"
 #[get("/player_channels_all")]
-pub async fn player_channels_all(channel_list: Data<RwLock<ChannelList>>) -> impl Responder {
-    HttpResponse::Ok().body(format!("{:?}", channel_list.read().await.values()))
+pub async fn player_channels_all(bancho: Data<Bancho>) -> impl Responder {
+    HttpResponse::Ok().body(format!("{:?}", bancho.channel_list.read().await.values()))
 }
 
 /// GET "/player_sessions_kick"
 #[get("/player_sessions_kick/{token}")]
-pub async fn player_sessions_kick(
-    token: Path<String>,
-    channel_list: Data<RwLock<ChannelList>>,
-    player_sessions: Data<RwLock<PlayerSessions>>,
-) -> impl Responder {
+pub async fn player_sessions_kick(token: Path<String>, bancho: Data<Bancho>) -> impl Responder {
     HttpResponse::Ok().body(
-        match player_sessions
+        match bancho
+            .player_sessions
             .write()
             .await
-            .logout(&token.0, Some(&channel_list))
+            .logout(&token.0, Some(&bancho.channel_list))
             .await
         {
             Some(player) => format!("{}\n{:?}", token.0, player),
@@ -178,16 +171,13 @@ pub async fn player_sessions_kick(
 
 /// GET "/player_sessions_kick_uid"
 #[get("/player_sessions_kick_uid/{user_id}")]
-pub async fn player_sessions_kick_uid(
-    user_id: Path<i32>,
-    channel_list: Data<RwLock<ChannelList>>,
-    player_sessions: Data<RwLock<PlayerSessions>>,
-) -> impl Responder {
+pub async fn player_sessions_kick_uid(user_id: Path<i32>, bancho: Data<Bancho>) -> impl Responder {
     HttpResponse::Ok().body(
-        match player_sessions
+        match bancho
+            .player_sessions
             .write()
             .await
-            .logout_with_id(user_id.0, Some(&channel_list))
+            .logout_with_id(user_id.0, Some(&bancho.channel_list))
             .await
         {
             Some(player) => format!("{:?}", player),
@@ -214,24 +204,24 @@ pub async fn test_geo_ip(
 
 /// GET "/bancho_config_get"
 #[get("/bancho_config_get")]
-pub async fn bancho_config_get(bancho_config: Data<RwLock<BanchoConfig>>) -> impl Responder {
-    HttpResponse::Ok().body(format!("{:?}", bancho_config.read().await))
+pub async fn bancho_config_get(bancho: Data<Bancho>) -> impl Responder {
+    HttpResponse::Ok().body(format!("{:?}", bancho.config.read().await))
 }
 
 /// GET "/bancho_config_update"
 #[get("/bancho_config_update")]
 pub async fn bancho_config_update(
     database: Data<Database>,
-    bancho_config: Data<RwLock<BanchoConfig>>,
+    bancho: Data<Bancho>,
 ) -> impl Responder {
-    let result = bancho_config.write().await.update(&database).await;
+    let result = bancho.config.write().await.update(&database).await;
     HttpResponse::Ok().body(format!("{}", result))
 }
 
 /// GET "/osu_api_test"
 #[get("/osu_api_test")]
-pub async fn osu_api_test(osu_api: Data<RwLock<OsuApi>>) -> impl Responder {
-    let results = osu_api.write().await.test_all().await;
+pub async fn osu_api_test(bancho: Data<Bancho>) -> impl Responder {
+    let results = bancho.osu_api.write().await.test_all().await;
     HttpResponse::Ok()
         .content_type("application/json")
         .body(results)
@@ -239,15 +229,15 @@ pub async fn osu_api_test(osu_api: Data<RwLock<OsuApi>>) -> impl Responder {
 
 /// GET "/osu_api_all"
 #[get("/osu_api_all")]
-pub async fn osu_api_all(osu_api: Data<RwLock<OsuApi>>) -> impl Responder {
-    HttpResponse::Ok().body(format!("{:?}", osu_api.read().await))
+pub async fn osu_api_all(bancho: Data<Bancho>) -> impl Responder {
+    HttpResponse::Ok().body(format!("{:?}", bancho.osu_api.read().await))
 }
 
 /// GET "/osu_api_reload"
 #[get("/osu_api_reload")]
-pub async fn osu_api_reload(osu_api: Data<RwLock<OsuApi>>) -> impl Responder {
+pub async fn osu_api_reload(bancho: Data<Bancho>) -> impl Responder {
     let start = Instant::now();
-    osu_api.write().await.reload_clients().await;
+    bancho.osu_api.write().await.reload_clients().await;
     let end = start.elapsed();
     HttpResponse::Ok().body(format!("done in: {:?}", end))
 }
