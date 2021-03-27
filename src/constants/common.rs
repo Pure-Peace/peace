e6b62c86ec10af0ba3336cbf080dd01e3aa2eee6
Pure-Peace <940857703@qq.com>
2021-03-28 02:55:21 +0800

    update: big version.

--- src/constants/common.rs
+++ src/constants/common.rs
@@ -1,5 +1,5 @@
 pub const DB_VERSION: &str = "0.6.5";
-pub const PEACE_VERSION: &str = "0.3.5";
+pub const PEACE_VERSION: &str = "0.5.0";
 pub const PEACE_BANNER: &str = r#"
 
    .-.                            
