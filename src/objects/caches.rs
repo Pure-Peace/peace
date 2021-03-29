fc7660c09087d7430b98b7722b83c97bafc8caea
Pure-Peace <940857703@qq.com>
2021-03-30 00:52:25 +0800

    add: Caches methods

--- src/objects/caches.rs
+++ src/objects/caches.rs
@@ -3,6 +3,8 @@ use hashbrown::HashMap;
 
 use crate::types::{Argon2Cache, BeatmapsCache};
 
+use super::Beatmaps;
+
 pub struct Caches {
     pub beatmaps_cache: RwLock<BeatmapsCache>,
     pub argon2_cache: RwLock<Argon2Cache>,
@@ -14,5 +16,31 @@ impl Caches {
             beatmaps_cache: RwLock::new(HashMap::with_capacity(2000)),
             argon2_cache: RwLock::new(HashMap::with_capacity(1000)),
         }
+    }
+
+    #[inline(always)]
+    pub async fn cache_beatmap(&self, beatmap_md5: String, beatmaps: &Beatmaps) -> Option<Beatmaps> {
+        self.beatmaps_cache
+            .write()
+            .await
+            .insert(beatmap_md5, beatmaps.clone())
+    }
+
+    #[inline(always)]
+    pub async fn get_beatmap(&self, beatmap_md5: &String) -> Option<Beatmaps> {
+        self.beatmaps_cache.read().await.get(beatmap_md5).cloned()
+    }
+
+    #[inline(always)]
+    pub async fn cache_password(&self, argon2: &String, password: &String) -> Option<String> {
+        self.argon2_cache
+            .write()
+            .await
+            .insert(argon2.to_string(), password.to_string())
+    }
+
+    #[inline(always)]
+    pub async fn get_password(&self, argon2: &String) -> Option<String> {
+        self.argon2_cache.read().await.get(argon2).cloned()
     }
 }
