ff91f7eb67aea9a7e8425f33f099e9b3b47ff824
Pure-Peace <940857703@qq.com>
2021-04-26 20:50:05 +0800

    peace-objects v0.6.0: PeaceApi, pp_calc -> pp_server_api

--- peace-objects/src/lib.rs
+++ peace-objects/src/lib.rs
@@ -3,4 +3,7 @@ extern crate log;
 
 pub mod beatmaps;
 pub mod osu_api;
-pub mod pp_calc;
+#[cfg(feature = "peace_api")]
+pub mod peace_api;
+#[cfg(feature = "pp_server_api")]
+pub mod pp_server_api;
---
+++ peace-objects/src/peace_api.rs
@@ -0,0 +1,114 @@
+use derivative::Derivative;
+use peace_constants::api::ApiError;
+use reqwest::{RequestBuilder, Response};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    time::Duration,
+};
+
+const REQUEST_TIMEOUT: u64 = 15;
+
+#[derive(Derivative)]
+#[derivative(Debug)]
+pub struct PeaceApi {
+    pub key: String,
+    pub url: String,
+    #[derivative(Debug = "ignore")]
+    pub client: reqwest::Client,
+    pub delay: AtomicUsize,
+    pub success_count: AtomicUsize,
+    pub failed_count: AtomicUsize,
+}
+
+impl PeaceApi {
+    #[inline(always)]
+    pub fn new(key: String, url: String) -> Self {
+        let client = reqwest::Client::builder()
+            .connection_verbose(false)
+            .timeout(Duration::from_secs(REQUEST_TIMEOUT))
+            .pool_idle_timeout(Some(Duration::from_secs(300)))
+            .build()
+            .unwrap();
+        Self {
+            key,
+            url,
+            client,
+            delay: AtomicUsize::new(0),
+            success_count: AtomicUsize::new(0),
+            failed_count: AtomicUsize::new(0),
+        }
+    }
+
+    #[inline(always)]
+    pub async fn request_wrapper(&self, req: RequestBuilder) -> Option<Response> {
+        let start = std::time::Instant::now();
+        let res = req.header("peace_key", &self.key).send().await;
+        let delay = start.elapsed().as_millis() as usize;
+        if res.is_err() {
+            warn!(
+                "[PeaceApi/get] Failed, key ({}) request with: {}ms; err: {:?};",
+                self.key, delay, res
+            );
+            self.failed(delay);
+            return None;
+        }
+        // TODO: Statistics avg request time
+        debug!(
+            "[PeaceApi/get] Success get resp, key ({}) request with: {:?}ms;",
+            self.key, delay
+        );
+        self.success(delay);
+        Some(res.unwrap())
+    }
+
+    #[inline(always)]
+    pub async fn post<T: Serialize + ?Sized>(&self, url: &str, json: &T) -> Option<Response> {
+        self.request_wrapper(self.client.post(url).json(json)).await
+    }
+
+    #[inline(always)]
+    pub async fn get<T: Serialize + ?Sized>(&self, url: &str, query: &T) -> Option<Response> {
+        self.request_wrapper(self.client.get(url).query(query))
+            .await
+    }
+
+    #[inline(always)]
+    pub async fn get_json<Q: Serialize + ?Sized, T: DeserializeOwned>(
+        &self,
+        url: &str,
+        query: &Q,
+    ) -> Result<T, ApiError> {
+        let res = match self.get(url, query).await {
+            Some(r) => {
+                if !r.status().is_success() {
+                    return Err(ApiError::RequestError);
+                };
+                r
+            }
+            None => {
+                return Err(ApiError::RequestError);
+            }
+        };
+
+        match res.json().await {
+            Ok(value) => Ok(value),
+            Err(err) => {
+                error!("[PeaceApi] Could not parse data to json, err: {:?}", err);
+                Err(ApiError::ParseError)
+            }
+        }
+    }
+
+    #[inline(always)]
+    pub fn success(&self, delay: usize) {
+        self.success_count.fetch_add(1, Ordering::SeqCst);
+        self.delay.swap(delay, Ordering::SeqCst);
+    }
+
+    #[inline(always)]
+    pub fn failed(&self, delay: usize) {
+        self.failed_count.fetch_add(1, Ordering::SeqCst);
+        self.delay.swap(delay, Ordering::SeqCst);
+    }
+}
