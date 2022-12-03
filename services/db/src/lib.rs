be1aa5e1efe30dbf9e5a74a513c829062b9dd6ab
Pure-Peace <940857703@qq.com>
2022-12-03 22:55:40 +0800

    `db` service now using `peace_rpc`

--- services/db/src/lib.rs
+++ services/db/src/lib.rs
@@ -1,1 +1,35 @@
+pub mod cfg;
 pub mod rpc;
+
+use cfg::DbConfig;
+use peace_pb::services::db::db_rpc_server::DbRpcServer;
+use peace_rpc::{cfg::RpcFrameConfig, Application};
+use rpc::Db;
+use std::sync::Arc;
+use tonic::transport::{server::Router, Server};
+
+#[derive(Clone)]
+pub struct App {
+    pub cfg: Arc<DbConfig>,
+}
+
+impl App {
+    pub fn new(cfg: Arc<DbConfig>) -> Self {
+        Self { cfg }
+    }
+}
+
+impl Application for App {
+    fn frame_cfg(&self) -> &RpcFrameConfig {
+        &self.cfg.frame_cfg
+    }
+
+    fn service_descriptor(&self) -> Option<&[u8]> {
+        todo!()
+    }
+
+    fn service(&self, mut configured_server: Server) -> Router {
+        let svc = Db::default();
+        configured_server.add_service(DbRpcServer::new(svc))
+    }
+}
--- services/db/src/main.rs
+++ services/db/src/main.rs
@@ -1,21 +1,12 @@
-use peace_db::rpc::Db;
-use peace_pb::services::db::{db_rpc_server::DbRpcServer, DB_DESCRIPTOR_SET};
-use tonic::transport::Server;
+use peace_db::{cfg::DbConfig, App};
+use peace_rpc::server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let svc = Db::default();
-    let reflect_svc = tonic_reflection::server::Builder::configure()
-        .register_encoded_file_descriptor_set(DB_DESCRIPTOR_SET)
-        .build()
-        .unwrap();
+    let app = App::new(DbConfig::get());
+    peace_logs::init(&app.cfg.frame_cfg);
 
-    Server::builder()
-        .add_service(reflect_svc)
-        .add_service(DbRpcServer::new(svc))
-        .serve(addr)
-        .await?;
+    server::serve(app).await;
 
     Ok(())
 }
