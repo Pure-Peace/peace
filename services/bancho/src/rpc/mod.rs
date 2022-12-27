de1a703455857f6ce58cf78b628d7748386583e9
Pure-Peace <purepeace_@outlook.com>
2022-12-27 16:25:07 +0800

    Init `bancho` service

--- services/bancho/src/rpc/mod.rs
+++ services/bancho/src/rpc/mod.rs
@@ -1,5 +1,8 @@
 use peace_pb::services::bancho::{
-    bancho_rpc_server::BanchoRpc, HelloReply, HelloRequest,
+    bancho_rpc_server::BanchoRpc, CommonReply, LobbyJoinRequest,
+    LobbyPartRequest, LoginReply, LoginRequest, PingRequest,
+    PresenceRequestAllRequest, RequestStatusUpdateRequest, RpcStatus,
+    SpectateCantRequest, SpectateStopRequest,
 };
 use tonic::{Request, Response, Status};
 
@@ -8,14 +11,122 @@ pub struct Bancho {}
 
 #[tonic::async_trait]
 impl BanchoRpc for Bancho {
-    async fn say_hello(
+    async fn ping(
         &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
+        request: Request<PingRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn login(
+        &self,
+        request: Request<LoginRequest>,
+    ) -> Result<Response<LoginReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = LoginReply {
+            status: RpcStatus::Ok.into(),
+            token: None,
+            protocol: None,
+            packet: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn request_status_update(
+        &self,
+        request: Request<RequestStatusUpdateRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn presence_request_all(
+        &self,
+        request: Request<PresenceRequestAllRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn spectate_stop(
+        &self,
+        request: Request<SpectateStopRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn spectate_cant(
+        &self,
+        request: Request<SpectateCantRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn lobby_part(
+        &self,
+        request: Request<LobbyPartRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn lobby_join(
+        &self,
+        request: Request<LobbyJoinRequest>,
+    ) -> Result<Response<CommonReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = CommonReply {
+            status: RpcStatus::Ok.into(),
+            packet: None,
+            err: None,
         };
 
         Ok(Response::new(reply))
