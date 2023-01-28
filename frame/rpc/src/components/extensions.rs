16be0bd2775c4b902868cae6e2a7fb66d809d039
Pure-Peace <purepeace_@outlook.com>
2023-01-29 05:41:05 +0800

    Simple `ClientIp` extrator for `peace_rpc`

--- frame/rpc/src/components/extensions.rs
+++ frame/rpc/src/components/extensions.rs
@@ -1,7 +1,18 @@
 use std::{net::IpAddr, ops::Deref};
+use tonic::{Request, Status};
 
 #[derive(Debug, Clone)]
 pub struct ClientIp(pub IpAddr);
+
+impl ClientIp {
+    pub fn from_request<T>(request: &Request<T>) -> Result<Self, Status> {
+        Ok(request
+            .extensions()
+            .get::<ClientIp>()
+            .ok_or(Status::internal("No client ip"))?
+            .to_owned())
+    }
+}
 
 impl Deref for ClientIp {
     type Target = IpAddr;
