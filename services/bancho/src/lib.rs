e7abd6aeb59ffe1a8975b08c2d4205438aa008cb
Pure-Peace <purepeace_@outlook.com>
2023-01-29 05:38:05 +0800

    The use case of the previous update

--- services/bancho/src/lib.rs
+++ services/bancho/src/lib.rs
@@ -1,14 +1,19 @@
 #[macro_use]
 extern crate peace_rpc;
+#[macro_use]
+extern crate peace_logs;
 
-pub mod impls;
+pub mod logic;
 pub mod rpc;
 
 use clap_serde_derive::ClapSerde;
-use peace_pb::services::bancho::{
-    bancho_rpc_server::BanchoRpcServer, BANCHO_DESCRIPTOR_SET,
+use peace_pb::services::{
+    bancho_rpc::bancho_rpc_server::BanchoRpcServer,
+    bancho_state_rpc::{self, BANCHO_STATE_DESCRIPTOR_SET},
 };
-use peace_rpc::{interceptor::client_ip, Application, RpcFrameConfig};
+use peace_rpc::{
+    interceptor::client_ip, Application, RpcClientConfig, RpcFrameConfig,
+};
 use rpc::Bancho;
 use std::sync::Arc;
 use tonic::{
@@ -16,12 +21,17 @@ use tonic::{
     transport::{server::Router, Server},
 };
 
+define_rpc_client_config!(service_name: bancho_state_rpc);
+
 /// Command Line Interface (CLI) for Bancho service.
 #[peace_config]
 #[command(name = "bancho", author, version, about, propagate_version = true)]
 pub struct BanchoConfig {
     #[command(flatten)]
     pub frame_cfg: RpcFrameConfig,
+
+    #[command(flatten)]
+    pub bancho_state: BanchoStateRpcConfig,
 }
 
 #[derive(Clone)]
@@ -42,11 +52,18 @@ impl Application for App {
     }
 
     fn service_descriptors(&self) -> Option<&[&[u8]]> {
-        Some(&[BANCHO_DESCRIPTOR_SET])
+        Some(&[BANCHO_STATE_DESCRIPTOR_SET])
     }
 
     async fn service(&self, mut configured_server: Server) -> Router {
-        let bancho = Bancho::default();
+        let bancho_state_rpc_client =
+            self.cfg.bancho_state.connect_client().await.unwrap_or_else(|err| {
+                error!("Unable to connect to the bancho_state gRPC service, please make sure the service is started.");
+                panic!("{}", err)
+            });
+
+        let bancho = Bancho::new(bancho_state_rpc_client);
+
         configured_server
             .add_service(BanchoRpcServer::with_interceptor(bancho, client_ip))
     }
