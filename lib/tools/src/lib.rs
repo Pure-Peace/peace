67da9c61e18244a742ca59e48609e03eecb533ce
Pure-Peace <purepeace_@outlook.com>
2023-01-28 15:41:31 +0800

    Add a `time_spent` macro for testing

--- lib/tools/src/lib.rs
+++ lib/tools/src/lib.rs
@@ -30,3 +30,58 @@ macro_rules! listening {
         format!("\n\n{}\n\n>> listening on {}", $crate::pkg_metadata!(), $addr)
     };
 }
+
+#[macro_export]
+/// Display code execution time
+///
+/// Examples:
+/// ```rust,ignore
+/// tools::time_spent!(
+///     @log=info;
+///     "hello".to_string();
+///     println!("hello")
+/// );
+///
+/// // or
+/// tools::time_spent!(
+///     @log=println;
+///     println!("hello")
+/// );
+///
+/// // or
+/// tools::time_spent!(
+///     @log=info;
+///     "this is a tag" => println!("hello"),
+///     "this is a tag1" => { let s = "".to_string(); }
+/// );
+///
+/// // or
+/// tools::time_spent!(
+///     @log=info;
+///     "tag for each" => @each(
+///         println!("hello"),
+///         println!("hello1")
+///     )
+/// );
+///
+/// ```
+macro_rules! time_spent {
+    (@log=$printer: tt; $($code: expr$(,)*)*) => {
+        $(
+            $crate::time_spent!(@log=$printer; stringify!($code) => $code);
+        )*
+    };
+    (@log=$printer: tt; $tag: expr => @each($($code: expr$(,)*)*)) => {
+        $(
+            $crate::time_spent!(@log=$printer; $tag => $code);
+        )*
+    };
+    (@log=$printer: tt; $($tag: expr => $code: expr$(,)*)*) => {
+        $(
+            let __time_start = std::time::Instant::now();
+            $code;
+            let __time_end = __time_start.elapsed();
+            $printer!(concat!("\"", $tag, "\" in {:?}"), __time_end);
+        )*
+    };
+}
