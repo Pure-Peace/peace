0db7c140eb1f04e73b98d0befbd8a0e1d491cb8a
Pure-Peace <purepeace_@outlook.com>
2023-03-02 19:40:00 +0800

    Add serde attrs to tonic generated codes

--- core/pb/build.rs
+++ core/pb/build.rs
@@ -2,12 +2,26 @@ use std::{env, path::PathBuf};
 
 const OUT_DIR: &str = "generated";
 
+macro_rules! define_attr {
+    ($const: ident, $($attr: pat$(,)*)*) => {
+        const $const: &str = concat!("#[derive(", $(stringify!($attr,)),*, ")]");
+    };
+}
+
+define_attr!(SERDE, serde::Deserialize, serde::Serialize);
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    build("logs");
-    build("peace_db");
-    build("chat");
-    build("bancho");
-    build("bancho_state");
+    build("logs", None);
+    build("peace_db", None);
+    build("chat", None);
+    build("bancho", None);
+    build(
+        "bancho_state",
+        Some(&[(
+            SERDE,
+            &["UserData", "ConnectionInfo", "GetAllSessionsResponse"],
+        )]),
+    );
 
     Ok(())
 }
@@ -20,8 +34,8 @@ fn proto(pkg: &str) -> String {
     format!("proto/{}.proto", pkg)
 }
 
-fn build(pkg: &str) {
-    configure()
+fn build(pkg: &str, type_attrs: Option<&[(&str, &[&str])]>) {
+    configure(type_attrs)
         .file_descriptor_set_path(descriptor(pkg))
         .compile(&[proto(pkg)], &["proto"])
         .unwrap();
@@ -35,6 +49,16 @@ fn out_dir() -> PathBuf {
     dir
 }
 
-fn configure() -> tonic_build::Builder {
-    tonic_build::configure().out_dir(out_dir())
+fn configure(type_attrs: Option<&[(&str, &[&str])]>) -> tonic_build::Builder {
+    let mut cfg = tonic_build::configure().out_dir(out_dir());
+
+    if let Some(type_attrs) = type_attrs {
+        for (attr, paths) in type_attrs {
+            for path in paths.iter() {
+                cfg = cfg.type_attribute(path, attr);
+            }
+        }
+    }
+
+    cfg
 }
