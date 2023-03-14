595271288c949fb05cdec582d484fcf91849ca53
Pure-Peace <purepeace_@outlook.com>
2023-03-14 16:37:02 +0800

    Add atomic operation traits

--- lib/tools/src/atomic.rs
+++ lib/tools/src/atomic.rs
@@ -7,6 +7,30 @@ use std::{
     sync::{atomic::*, Arc},
 };
 
+pub trait AtomicBitOperation: AtomicValue {
+    fn and(&self, val: Self::Value) -> Self::Value;
+    fn nand(&self, val: Self::Value) -> Self::Value;
+    fn or(&self, val: Self::Value) -> Self::Value;
+    fn xor(&self, val: Self::Value) -> Self::Value;
+
+    fn and_eq(&self, val: Self::Value);
+    fn nand_eq(&self, val: Self::Value);
+    fn or_eq(&self, val: Self::Value);
+    fn xor_eq(&self, val: Self::Value);
+}
+
+pub trait AtomicOperation: AtomicValue {
+    fn add(&self, val: Self::Value) -> Self::Value;
+    fn max(&self, val: Self::Value) -> Self::Value;
+    fn min(&self, val: Self::Value) -> Self::Value;
+    fn sub(&self, val: Self::Value) -> Self::Value;
+
+    fn add_eq(&self, val: Self::Value);
+    fn max_eq(&self, val: Self::Value);
+    fn min_eq(&self, val: Self::Value);
+    fn sub_eq(&self, val: Self::Value);
+}
+
 pub trait AtomicValue: Sized {
     type Value;
     fn val(&self) -> Self::Value;
@@ -17,16 +41,19 @@ pub type Atomic<T> = AtomicAny<Arc<T>>;
 pub type AtomicOption<T> = AtomicAny<Option<Arc<T>>>;
 
 impl<T> Atomic<T> {
+    #[inline]
     pub fn new(val: T) -> Self {
         Self(Arc::new(val).into())
     }
 }
 
 impl<T> AtomicOption<T> {
+    #[inline]
     pub fn new(val: T) -> Self {
         Self(Some(Arc::new(val)).into())
     }
 
+    #[inline]
     pub fn from_option(option: Option<T>) -> Self {
         Self(option.map(|inner| inner.into()).into())
     }
@@ -81,12 +108,14 @@ where
 }
 
 impl Into<Atomic<String>> for String {
+    #[inline]
     fn into(self) -> Atomic<String> {
         Atomic::new(self)
     }
 }
 
 impl Into<AtomicOption<String>> for Option<String> {
+    #[inline]
     fn into(self) -> AtomicOption<String> {
         AtomicOption::from_option(self)
     }
@@ -101,10 +130,12 @@ where
 {
     type Value = T;
 
+    #[inline]
     fn val(&self) -> Self::Value {
         self.0.load_full()
     }
 
+    #[inline]
     fn set(&self, val: Self::Value) {
         self.0.store(val)
     }
@@ -137,10 +168,12 @@ macro_rules! implAtomicValue {
                 impl AtomicValue for [<$ty:camel>] {
                     type Value = [<$ty:snake>];
 
+                    #[inline]
                     fn val(&self) -> Self::Value {
                         self.0.load(Ordering::SeqCst)
                     }
 
+                    #[inline]
                     fn set(&self, val: Self::Value) {
                         self.0.store(val, Ordering::SeqCst)
                     }
@@ -161,6 +194,7 @@ macro_rules! implAtomicValue {
                 }
 
                 impl Into<[<$ty:camel>]> for [<$ty:snake>] {
+                    #[inline]
                     fn into(self) -> [<$ty:camel>] {
                         [<$ty:camel>]::new(self)
                     }
@@ -192,3 +226,108 @@ macro_rules! implAtomicValueSerde {
 }
 
 implAtomicValueSerde!(bool, i8, u8, i16, u16, i32, u32, i64, u64, f32, f64);
+
+macro_rules! implAtomicOperation {
+    ($($ty: ty$(,)*)*) => {
+        paste::paste! {
+            $(
+                impl AtomicOperation for [<$ty:camel>] {
+                    #[inline]
+                    fn add(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_add(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn max(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_max(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn min(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_min(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn sub(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_sub(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn add_eq(&self, val: Self::Value) {
+                        self.set(self.add(val));
+                    }
+
+                    #[inline]
+                    fn max_eq(&self, val: Self::Value) {
+                        self.set(self.max(val));
+                    }
+
+                    #[inline]
+                    fn min_eq(&self, val: Self::Value) {
+                        self.set(self.min(val));
+                    }
+
+                    #[inline]
+                    fn sub_eq(&self, val: Self::Value) {
+                        self.set(self.sub(val));
+                    }
+
+                }
+            )*
+        }
+    };
+}
+
+implAtomicOperation!(i8, u8, i16, u16, i32, u32, i64, u64, isize, usize, f32, f64);
+
+macro_rules! implAtomicBitOperation {
+    ($($ty: ty$(,)*)*) => {
+        paste::paste! {
+            $(
+                impl AtomicBitOperation for [<$ty:camel>] {
+                    #[inline]
+                    fn and(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_and(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn nand(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_nand(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn or(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_or(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn xor(&self, val: Self::Value) -> Self::Value {
+                        self.0.fetch_xor(val, Ordering::SeqCst)
+                    }
+
+                    #[inline]
+                    fn and_eq(&self, val: Self::Value) {
+                        self.set(self.and(val));
+                    }
+
+                    #[inline]
+                    fn nand_eq(&self, val: Self::Value) {
+                        self.set(self.nand(val));
+                    }
+
+                    #[inline]
+                    fn or_eq(&self, val: Self::Value) {
+                        self.set(self.or(val));
+                    }
+
+                    #[inline]
+                    fn xor_eq(&self, val: Self::Value) {
+                        self.set(self.xor(val));
+                    }
+                }
+            )*
+        }
+    };
+}
+
+implAtomicBitOperation!(i8, u8, i16, u16, i32, u32, i64, u64, isize, usize);
