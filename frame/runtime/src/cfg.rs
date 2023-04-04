5c89a9ed8d7aa2a949ee20449259ac1b1c7983f6
Pure-Peace <purepeace_@outlook.com>
2023-04-05 03:12:20 +0800

    Fix `RuntimeConfig` default value

--- frame/runtime/src/cfg.rs
+++ frame/runtime/src/cfg.rs
@@ -39,15 +39,15 @@ pub struct RuntimeConfig {
     /// Enables the I/O driver.
     ///
     /// Doing this enables using net, process, signal, and some I/O types on the runtime.
-    #[default(false)]
-    #[arg(long)]
+    #[default(true)]
+    #[arg(long, default_value = "true")]
     pub runtime_enable_io: bool,
 
     /// Enables the time driver.
     ///
     /// Doing this enables using `tokio::time` on the runtime.
-    #[default(false)]
-    #[arg(long)]
+    #[default(true)]
+    #[arg(long, default_value = "true")]
     pub runtime_enable_time: bool,
 
     /// Enables both I/O and time drivers.
@@ -55,8 +55,8 @@ pub struct RuntimeConfig {
     /// Doing this is a shorthand for calling enable_io and enable_time individually.
     /// If additional components are added to Tokio in the future,
     /// enable_all will include these future components.
-    #[default(false)]
-    #[arg(long)]
+    #[default(true)]
+    #[arg(long, default_value = "true")]
     pub runtime_enable_all: bool,
 
     /// Sets the number of scheduler ticks after which the scheduler will poll for external events (timers, I/O, and so on).
