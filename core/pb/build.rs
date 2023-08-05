298e72b1bfcc6b51cd42e442846a9110669da520
Pure-Peace <purepeace_@outlook.com>
2023-08-06 01:25:51 +0800

    Add `peace_proto_build` and fix proto build

--- core/pb/build.rs
+++ core/pb/build.rs
@@ -1,41 +1,21 @@
 #![allow(dead_code)]
-
-use std::{env, path::PathBuf};
-
-const OUT_DIR: &str = "generated";
-
-macro_rules! define_attr {
-    ($const: ident, $($attr: pat$(,)*)*) => {
-        const $const: &str = concat!("#[derive(", $(stringify!($attr,)),*, ")]");
-    };
-}
-
-define_attr!(SERDE, serde::Deserialize, serde::Serialize);
+use peace_proto_build::{preset_addr::SERDE, ProtoBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(feature = "base")]
-    build("base", None);
+    let builder = ProtoBuilder::new("proto", "peace", "generated");
 
-    #[cfg(feature = "logs")]
-    build("frame.logs", None);
-
-    #[cfg(feature = "chat")]
-    build("services.chat", None);
-
-    #[cfg(feature = "bancho")]
-    build("services.bancho", None);
-
-    #[cfg(feature = "bancho_state")]
-    build(
+    builder.build("base", None);
+    builder.build("frame.logs", None);
+    builder.build("services.chat", None);
+    builder.build("services.bancho", None);
+    builder.build(
         "services.bancho_state",
         Some(&[(
             SERDE,
             &["UserData", "ConnectionInfo", "GetAllSessionsResponse"],
         )]),
     );
-
-    #[cfg(feature = "geoip")]
-    build(
+    builder.build(
         "services.geoip",
         Some(&[(
             SERDE,
@@ -51,45 +31,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )]),
     );
 
-    #[cfg(feature = "signature")]
-    build("services.signature", None);
+    builder.build("services.signature", None);
 
     Ok(())
-}
-
-fn descriptor(pkg: &str) -> PathBuf {
-    out_dir().join(format!("peace.{}.descriptor.bin", pkg))
-}
-
-fn proto(pkg: &str) -> String {
-    format!("proto/peace/{}.proto", pkg)
-}
-
-fn build(pkg: &str, type_attrs: Option<&[(&str, &[&str])]>) {
-    configure(type_attrs)
-        .file_descriptor_set_path(descriptor(pkg))
-        .compile(&[proto(pkg.replace('.', "/").as_str())], &["proto"])
-        .unwrap();
-}
-
-fn out_dir() -> PathBuf {
-    let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(OUT_DIR);
-    if !dir.exists() {
-        std::fs::create_dir_all(dir.clone()).unwrap();
-    }
-    dir
-}
-
-fn configure(type_attrs: Option<&[(&str, &[&str])]>) -> tonic_build::Builder {
-    let mut cfg = tonic_build::configure().out_dir(out_dir());
-
-    if let Some(type_attrs) = type_attrs {
-        for (attr, paths) in type_attrs {
-            for path in paths.iter() {
-                cfg = cfg.type_attribute(path, attr);
-            }
-        }
-    }
-
-    cfg
 }
---
+++ lib/proto_build/src/lib.rs
@@ -0,0 +1,80 @@
+use std::path::PathBuf;
+
+pub const PROTO_EXT: &str = "proto";
+pub const DESCRIPTOR_EXT: &str = "descriptor.bin";
+
+#[derive(Debug, Clone)]
+pub struct ProtoBuilder {
+    pub proto_root: String,
+    pub namespace: String,
+    pub out_dir: String,
+}
+
+impl ProtoBuilder {
+    pub fn new<S: Into<String>>(
+        proto_root: S,
+        namespace: S,
+        out_dir: S,
+    ) -> Self {
+        Self {
+            proto_root: proto_root.into(),
+            namespace: namespace.into(),
+            out_dir: out_dir.into(),
+        }
+    }
+
+    pub fn descriptor_output(&self, pkg: &str) -> PathBuf {
+        self.out_dir()
+            .join(format!("{}.{pkg}.{DESCRIPTOR_EXT}", self.namespace))
+    }
+
+    pub fn proto_path(&self, pkg: &str) -> PathBuf {
+        PathBuf::new()
+            .join(&self.proto_root)
+            .join(&self.namespace)
+            .join(format!("{}.{PROTO_EXT}", pkg.replace('.', "/")))
+    }
+
+    pub fn build(&self, pkg: &str, type_attrs: Option<&[(&str, &[&str])]>) {
+        self.configure(type_attrs)
+            .file_descriptor_set_path(self.descriptor_output(pkg))
+            .compile(&[self.proto_path(pkg)], &[&self.proto_root])
+            .unwrap();
+    }
+
+    pub fn out_dir(&self) -> PathBuf {
+        let path = PathBuf::from(&self.out_dir);
+        if !path.exists() {
+            std::fs::create_dir_all(&path).unwrap();
+        }
+        path
+    }
+
+    pub fn configure(
+        &self,
+        type_attrs: Option<&[(&str, &[&str])]>,
+    ) -> tonic_build::Builder {
+        let mut cfg = tonic_build::configure().out_dir(self.out_dir());
+
+        if let Some(type_attrs) = type_attrs {
+            for (attr, paths) in type_attrs {
+                for path in paths.iter() {
+                    cfg = cfg.type_attribute(path, attr);
+                }
+            }
+        }
+
+        cfg
+    }
+}
+
+#[macro_export]
+macro_rules! define_attr {
+    ($const: ident, $($attr: pat$(,)*)*) => {
+        pub const $const: &str = concat!("#[derive(", $(stringify!($attr,)),*, ")]");
+    };
+}
+
+pub mod preset_addr {
+    define_attr!(SERDE, serde::Deserialize, serde::Serialize);
+}
